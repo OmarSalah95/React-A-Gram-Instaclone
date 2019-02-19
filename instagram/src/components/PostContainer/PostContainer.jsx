d4dc37d60435b2e2c2e00237860b52339fec5e18
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-02-18 17:16:31 -0800

    Components are now being built and rendered to the browser window

---
+++ instagram/src/components/PostContainer/PostContainer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import CommentSection from '../CommentSection/CommentSection';
+import './PostContainer.css';
+
+const PostContainer = props => {
+  return (
+    <div className="post-container">
+      <div className="post-header">
+        <div className="post-thumb-wrapper">
+          <img
+          alt="post header"
+          className="post-thumb"
+          src={props.post.thumbnailUrl}
+          />
+        </div>
+        <h2>
+          <a>@{props.post.username}</a>
+        </h2>
+      </div>
+      <div className="post-image-wrapper">
+        <img
+          alt="post thumbnail"
+          className="post-image"
+          src={props.post.imageUrl}
+        />
+      </div>
+        <CommentSection comments={props.post.comments} />
+    </div>
+  );
+};
+
+export default PostContainer;
